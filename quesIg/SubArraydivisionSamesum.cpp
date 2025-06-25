356878f2559c0edc29bcd3132b2b55e1ee028c06
Abhishek Singh <abhi.r1509.git@gmail.com>
2025-06-25 22:35:07 +0530

    subarray division into 2

---
+++ quesIg/SubArraydivisionSamesum.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define fastio() ios::sync_with_stdio(false); cin.tie(0);
+using namespace std;
+
+bool division_of_array(vector<int> & arr){
+    int n ,sum;
+    n = arr.size();
+    sum = 0;
+    for(int i = 0;i<n;i++){
+        sum += arr[i];
+    }
+    int prefix = 0,ans;
+    for(int i=0;i<n-1;i++){
+        prefix += arr[i];
+        ans = sum - prefix;
+        if(ans==prefix) return true;
+    }
+    return false;  
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0;i < n;i++){
+        cin >> a[i];
+    }
+
+    cout << division_of_array(a);
+
+    return 0;
+}
